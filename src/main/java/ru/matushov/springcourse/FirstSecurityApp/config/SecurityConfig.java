bec119a4d799c75a296c05c816b5697ef4fd38b3
Zorgit <piligrimexe@Gmail.com>
2023-05-06 13:02:39 +0300

    Configure custom auth web form

--- src/main/java/ru/matushov/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/matushov/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.matushov.springcourse.FirstSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -19,7 +20,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.personDetailsService = personDetailsService;
     }
 
+    //Override auth webview, rules
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //config spring security
+        //configure auth
+        http.csrf().disable() //TODO enable csrf protect
+                .authorizeRequests()
+                .antMatchers("/auth/login","/error").permitAll() //access granted to all
+                .anyRequest().authenticated() //only after auth
+                .and()//go to configure
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login")
+                .defaultSuccessUrl("/hello", true)
+                .failureUrl("/auth/login?error");
+    }
+
     //Config authentication
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
