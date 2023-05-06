3a9dfab4bcecb0c89adf4b7f7ccc5e078cea48d8
Zorgit <piligrimexe@Gmail.com>
2023-05-06 12:20:02 +0300

    Give aut rules to Spring Security

--- src/main/java/ru/matushov/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/matushov/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -1,24 +1,33 @@
 package ru.matushov.springcourse.FirstSecurityApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.matushov.springcourse.FirstSecurityApp.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.matushov.springcourse.FirstSecurityApp.services.PersonDetailsService;
 
 //https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     //Config authentication
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    //TODO PasswordEncode
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
