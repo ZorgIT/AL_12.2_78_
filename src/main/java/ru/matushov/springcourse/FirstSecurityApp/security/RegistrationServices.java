a746e68adaa81c9467a96dcf124a6fcf9998f527
Zorgit <piligrimexe@Gmail.com>
2023-05-06 14:27:26 +0300

    add encryption password

--- src/main/java/ru/matushov/springcourse/FirstSecurityApp/security/RegistrationServices.java
+++ src/main/java/ru/matushov/springcourse/FirstSecurityApp/security/RegistrationServices.java
@@ -1,6 +1,7 @@
 package ru.matushov.springcourse.FirstSecurityApp.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.matushov.springcourse.FirstSecurityApp.models.Person;
@@ -9,14 +10,18 @@ import ru.matushov.springcourse.FirstSecurityApp.repositories.PeopleRepository;
 @Service
 public class RegistrationServices {
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationServices(PeopleRepository peopleRepository) {
+    public RegistrationServices(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+        String encodedPassword = passwordEncoder.encode(person.getPassword());
+        person.setPassword(encodedPassword);//
         peopleRepository.save(person);
     }
 }
